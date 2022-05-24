1594a6b28eac04d5e639fab2bf5f4e96214e7e9d
pankaj1707k <pankaj.1707k@gmail.com>
2022-05-24 14:32:15 +0530

    MongoDB connection using mongoose and .env setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const taskRoutes = require("./routes/tasks");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = 8000;
 
 // middleware
@@ -8,5 +10,9 @@ app.use(express.json());
 
 // routes
 app.use("/api/tasks", taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI).catch((err) => {
+  console.log(err);
+});
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
